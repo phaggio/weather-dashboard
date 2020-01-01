5360bd242a5d0bae5ac6b6e1f75a427a2540cfd4
Richard Wang <richardchacha@hotmail.com>
2020-01-01 00:38:57 -0800

    add offline UV index obj

--- script.js
+++ script.js
@@ -7,58 +7,23 @@ const temperatureDiv = $('#current-temperature');
 const detailDiv = $('#current-detail');
 
 // Weather API constant
-const currentWeather = 'http://api.openweathermap.org/data/2.5/weather?q='
+const currentWeather = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const forecastWeather = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 const key = '&APPID=786953f37f3a1158ba41f05aad533b5b';
 const searchCity = 'Seattle';
 
 // current weather Obj.
-let currentWeatherObj = {
-  "coord": {
-    "lon": -122.33
-    , "lat": 47.6
-  },
-  "weather": [
-    {
-      "id": 804,
-      "main": "Clouds",
-      "description": "overcast clouds",
-      "icon": "04n"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 280.8, // current temperature in K
-    "feels_like": 276.9,
-    "temp_min": 279.26,
-    "temp_max": 282.15,
-    "pressure": 1027,
-    "humidity": 76
-  },
-  "visibility": 16093,
-  "wind": {
-    "speed": 3.6, // meters per second
-    "deg": 160 // degree, 160 is South-Southeast
-  },
-  "clouds": {
-    "all": 90
-  },
-  "dt": 1577756730, // Tuesday, December 31, 2019 1:45:30 AM UTC or Monday, December 30, 2019 5:45:30 PM GMT-08:00
-  "sys": {
-    "type": 1,
-    "id": 3417,
-    "country": "US",
-    "sunrise": 1577721444,
-    "sunset": 1577751955
-  },
-  "timezone": -28800,
-  "id": 5809844,
-  "name": "Seattle",
-  "cod": 200
-}
+let currentWeatherObj = {"coord":{"lon":-122.33,"lat":47.6},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],"base":"stations","main":{"temp":283.9,"feels_like":278.31,"temp_min":282.04,"temp_max":285.37,"pressure":1004,"humidity":81},"visibility":16093,"wind":{"speed":7.2,"deg":220,"gust":11.3},"clouds":{"all":75},"dt":1577867176,"sys":{"type":1,"id":3417,"country":"US","sunrise":1577894250,"sunset":1577924861},"timezone":-28800,"id":5809844,"name":"Seattle","cod":200};
 
-// update current weather div
-// function updateCurrentWeather() {
+let currentUVObj = {
+  "lat": 47.6,
+  "lon": -122.33, 
+  "date_iso": "2020-01-01T12:00:00Z", 
+  "date": 1577880000, 
+  "value": 0.58
+};
+
+// parsed Obj values
 let city = currentWeatherObj.name;
 let country = currentWeatherObj.sys.country;
 let currentDate = convertUTC(currentWeatherObj.dt, currentWeatherObj.timezone);
@@ -74,8 +39,9 @@ let sunrise = convertUTC(currentWeatherObj.sys.sunrise, currentWeatherObj.timezo
 let sunset = convertUTC(currentWeatherObj.sys.sunset, currentWeatherObj.timezone).format("h:mm a");
 let humidity = currentWeatherObj.main.humidity + '%';
 let windSpeed = mpsToMph(currentWeatherObj.wind.speed);
-let uvIndex;
-// }
+let uvIndex = currentUVObj.value;
+
+console.log(city, currentDate.format('MM-DD h:mm a'), currentTemp);
 console.log(sunrise, sunset, humidity);
 console.log(windSpeed);
 
